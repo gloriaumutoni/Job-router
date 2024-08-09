afde0d1375ac3654429f8e1530707f2a7f21847f
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-08-09 16:40:26 +0200

    updated home and about page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='about' element={<About/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
