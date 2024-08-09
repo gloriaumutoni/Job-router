8673705a69da005d0784efa5e754f62e7617154a
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-08-09 17:42:01 +0200

    using createBrowserRouter to render elements

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Home from './pages/home'
-import About from './pages/about'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
 
 function App() {
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
-    </Routes>
-    </BrowserRouter>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    )
   );
+  return (
+    /* <header>
+      <nav>
+        <NavLink to='/'>Home</NavLink>
+        <NavLink to='about'>About</NavLink>
+      </nav>
+    </header> */
+    <RouterProvider router={router}/>
+  )
+  
 }
 
-export default App
+export default App;
